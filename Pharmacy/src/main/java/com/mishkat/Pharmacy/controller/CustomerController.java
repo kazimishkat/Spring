800be8c25bc98c62b4deb97630e532290575a0af
Kazi Mishkatul Islam <66875314+kazimishkat@users.noreply.github.com>
2026-06-15 14:16:41 +0600

    add

--- Pharmacy/src/main/java/com/mishkat/Pharmacy/controller/CustomerController.java
+++ Pharmacy/src/main/java/com/mishkat/Pharmacy/controller/CustomerController.java
@@ -16,8 +16,30 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping
-    private ResponseEntity<Customer> save(@RequestBody Customer c){
-        Customer savedCustomer=customerService.save(c);
-        return ResponseEntity.ok(savedCustomer);
+    private ResponseEntity<Map<String,String>> save(@RequestPart(value = "customer") String data, @RequestParam(value = "image") MultipartFile file){
+        ObjectMapper objectMapper = new ObjectMapper();
+        Customer cu = objectMapper.readValue(data, Customer.class);
+
+        try{
+            customerService.save(cu, file);
+            Map<String, String> response = new HashMap<>();
+            response.put("Message", "User Added Successfully ");
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+
+        }
+        catch (Exception e){
+
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("Message", "User Add Faild " + e);
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getALl(){
+        List<Customer> list = customerService.findAll();
+        return  ResponseEntity.ok(list);
+
     }
 }
